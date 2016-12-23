c74ad6c979586e87052eff594cfa2e36bb916a4f
Jazeps Basko <jazeps.basko@gmail.com>
2016-12-23 01:47:26 +0000

    Version bump

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setup(
     name='pytest-random-order',
-    version='0.2.1',
+    version='0.2.2',
     author='Jazeps Basko',
     author_email='jazeps.basko@gmail.com',
     maintainer='Jazeps Basko',
