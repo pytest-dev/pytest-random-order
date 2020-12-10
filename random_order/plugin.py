bc6b91bed9b5fb529507274a34c2e965b8bf9ba6
yashsaboo <yashsaboo99@gmail.com>
2020-12-09 21:02:44 -0600

    Cleaned the code by removing empty spaces

--- random_order/plugin.py
+++ random_order/plugin.py
@@ -2,6 +2,7 @@ import random
 import sys
 import traceback
 import warnings
+from collections import OrderedDict
 
 import pytest
 
@@ -9,7 +10,7 @@ from random_order.bucket_types import bucket_type_keys, bucket_types
 from random_order.cache import process_failed_first_last_failed
 from random_order.config import Config
 from random_order.shuffler import _disable, _get_set_of_item_ids, _shuffle_items
-from collections import OrderedDict 
+
 
 def pytest_addoption(parser):
     group = parser.getgroup('pytest-random-order options')
@@ -46,15 +47,13 @@ def pytest_addoption(parser):
         action='store',
         dest='flaky_test_log_path',
         default="flaky_test.json",
-        help='To set the file path for storing the flaky test log file',
+        help='To set the file path for storing the flaky test log file.',
     )
 
 def pytest_configure(config):
     config.addinivalue_line(
         'markers',
-        'random_order(disabled=True): disable reordering of tests within a module or class'# ,
-        # 'flaky-test-finder(n): run the given set of tests in random order `n` times',
-        # 'flaky-test-log-path: set the file path for storing the flaky test log file'
+        'random_order(disabled=True): disable reordering of tests within a module or class'
     )
 
 def pytest_report_header(config):
@@ -63,9 +62,9 @@ def pytest_report_header(config):
         return "Test order randomisation NOT enabled. Enable with --random-order or --random-order-bucket=<bucket_type>"
     return_string = ""
     if plugin.is_enabled and plugin.flaky_test_finder <= 1:
-        return_string = return_string + "Flaky test finder NOT enabled. Enable with --flaky-test-finder=<repition_number> where repition_number>1\n"
-    if plugin.flaky_test_log_path:
-        return_string = return_string + "Flaky test log path NOT enabled. Enable with --flaky-test-log-path=<log_path>\n"
+        return_string = return_string + "Flaky test finder NOT enabled. Enable with --flaky-test-finder = <repition_number> where repition_number > 1\n"
+    if plugin.is_enabled and plugin.flaky_test_log_path:
+        return_string = return_string + "Flaky test log path NOT enabled. Enable with --flaky-test-log-path = <log_path>\n"
     return return_string + (
         'Using --random-order-bucket={plugin.bucket_type}\n'
         'Using --random-order-seed={plugin.seed}\n'
@@ -92,19 +91,13 @@ def pytest_collection_modifyitems(session, config, items):
                 seed=seed,
                 session=session,
             )
-        
-        # print("Shuffle Doneeeee")
-        # print("\nItems:", items)
-        
+
         if plugin.flaky_test_finder>1:
             new_items = reorder_based_on_the_test_set(items)
             # Deep Copy is required
             for idx, item in enumerate(new_items):
                 items[idx] = item
-            # print("\nReordering done based on test_set")
 
-        # print("\nItems:", items)
-        
     except Exception as e:
         # See the finally block -- we only fail if we have lost user's tests.
         _, _, exc_tb = sys.exc_info()
@@ -123,6 +116,7 @@ def pytest_collection_modifyitems(session, config, items):
                 failure = 'pytest-random-order plugin has failed miserably'
             raise RuntimeError(failure)
 
+# Inspired from pytest-repeat plugin
 @pytest.hookimpl(trylast=True)
 def pytest_generate_tests(metafunc):
     plugin = Config(metafunc.config)
@@ -143,8 +137,6 @@ def pytest_report_teststatus(report, config):
             prefix = 'flaky-repeat_'
             suffix = ']'
             original_test_name = report.nodeid.split('[')[0]
-            # print("\noriginal_test_name: ",original_test_name)
-            # print("\nreport.nodeid: ",report.nodeid)
             repeat_number = int(report.nodeid[report.nodeid.find(prefix)+len(prefix) : report.nodeid.find(suffix)][0]) - 1
 
             # Check if current test is flaky or not
@@ -165,12 +157,12 @@ def pytest_report_teststatus(report, config):
 
 def pytest_terminal_summary(terminalreporter, exitstatus, config):
     plugin = Config(config)
-    if plugin.flaky_test_finder>1:
+    if plugin.is_enabled and plugin.flaky_test_finder>1:
         if len(set_of_flaky_tests):
             print("\nList of Flaky Tests: ", set_of_flaky_tests)
         else:
             print("\nList of Flaky Tests: None")
-        
+
         #Extract flaky test ordering log data
         flaky_test_log_data = {}
         for flaky_test in set_of_flaky_tests:
@@ -198,9 +190,6 @@ def reorder_based_on_the_test_set(items):
     if items is None or len(items)==0:
         return items
 
-    # Get the ids of the item: If repeat was set to 2 then test id would be: fileName.py::testName[1-2] where 1 is repeat number and 2 is number of total repeats
-    # print("\nList of Item IDs:", list(item.nodeid for item in items))
-    
     try:
         # Sort in place based on number of times asked to repeat
         list_of_repeat = []
