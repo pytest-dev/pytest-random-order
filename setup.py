0e43376e190400558d6dc6326052231b57f24f59
Jazeps Basko <jazeps.basko@gmail.com>
2022-12-03 17:45:32 +0200

    Version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read(fname):
 
 setup(
     name='pytest-random-order',
-    version='1.0.4',
+    version='1.1.0',
     author='Jazeps Basko',
     author_email='jazeps.basko@gmail.com',
     maintainer='Jazeps Basko',
