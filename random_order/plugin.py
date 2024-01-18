b0bb6aee98431387bf51fa28663b14e0c2707ebf
Jhansi M N S <94177510+jhanm12@users.noreply.github.com>
2024-01-18 12:15:18 +0530

    Update plugin.py: Failed First Plugin should be processed only when Cache is Enabled

--- random_order/plugin.py
+++ random_order/plugin.py
@@ -69,7 +69,8 @@ def pytest_collection_modifyitems(session, config, items):
     failure = None
 
     session.random_order_bucket_type_key_handlers = []
-    process_failed_first_last_failed(session, config, items)
+    if config.cache:
+        process_failed_first_last_failed(session, config, items)
 
     item_ids = _get_set_of_item_ids(items)
 
